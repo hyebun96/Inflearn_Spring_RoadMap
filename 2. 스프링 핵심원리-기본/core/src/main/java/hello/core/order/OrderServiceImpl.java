1a1de524a9ee4c18bdacad08e5d6e05504c5bc1b
hyebun96 <hyebun96@naver.com>
2024-11-29 21:52:36 +0900

    feat: 주문과 할인 도메인 개발, 테스트

---
+++ 2. 스프링 핵심원리-기본/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,24 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+
+        // 단일 책임의 원칙 잘 적용한 예제
+        // 예를 들어, 할인을 변경할 시 할인 부분만 고치면 되기 때문에
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
