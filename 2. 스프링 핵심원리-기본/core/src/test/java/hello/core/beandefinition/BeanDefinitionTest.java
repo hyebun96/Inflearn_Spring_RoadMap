2132f8ec16f92ce19513be55c919c6f29531a28d
hyebun96 <hyebun96@naver.com>
2024-12-03 16:39:24 +0900

    feat: 스프링 빈 메타정보 Bean Definition

---
+++ 2. 스프링 핵심원리-기본/core/src/test/java/hello/core/beandefinition/BeanDefinitionTest.java
@@ -0,0 +1,30 @@
+package hello.core.beandefinition;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class BeanDefinitionTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    // Spring Bean 을 생성하는 두가지 방법
+    // 1. SpringBean 을 등록하는 방법 (XML)
+    // 2. Factory Bean 을 이용해 등록하는 방 (Java Config)
+
+    @Test
+    @DisplayName("빈 설정 메타 정보 확인")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for(String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if(beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                System.out.println("beanDefinitionName = " + beanDefinitionName
+                + " beanDefinition = " + beanDefinition);
+            }
+        }
+    }
+}
