0721ba24bf1dd462e8d5d05b3039e07e85579e2b
hyebun96 <hyebun96@naver.com>
2024-12-04 14:03:08 +0900

    feat: 컴포넌트 스캔 중복등록과 충돌

--- 2. 스프링 핵심원리-기본/core/src/main/java/hello/core/AutoAppConfig.java
+++ 2. 스프링 핵심원리-기본/core/src/main/java/hello/core/AutoAppConfig.java
@@ -11,4 +11,12 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+
+    // 수동 등록 빈이 우선권을 갖음
+    // Overriding bean definition for bean : 수동 빈이 자동빈 오버라이드
+    //    @Bean(name = "memoryMemberRepository")
+    //    MemberRepository memberRepository() {
+    //        return new MemoryMemberRepository();
+    //    }
+
 }
