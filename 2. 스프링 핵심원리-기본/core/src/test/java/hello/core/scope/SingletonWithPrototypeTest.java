3848b8fde67eeefbd0c07a0ab4ec046bfca84bcf
hyebun96 <hyebun96@naver.com>
2024-12-06 15:11:49 +0900

    refactor: 싱글톤 빈과 함께 사용할 때 Provider로 문제 해결

--- 2. 스프링 핵심원리-기본/core/src/test/java/hello/core/scope/SingletonWithPrototypeTest.java
+++ 2. 스프링 핵심원리-기본/core/src/test/java/hello/core/scope/SingletonWithPrototypeTest.java
@@ -3,6 +3,7 @@ package hello.core.scope;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -34,19 +35,17 @@ public class SingletonWithPrototypeTest {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count1).isEqualTo(2);
+        assertThat(count1).isEqualTo(1);
     }
 
     @Scope("singleton") // default 라 안해도 되지만 헷갈리니까.
     static class ClientBean {
-        private final PrototypeBean prototypeBean;      // 생성시점에 주입
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
